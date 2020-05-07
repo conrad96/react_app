1fdeff3f0ee6326f95779bfa9ecaacd20c6bafbe
conrad96 <conrad.mugisha@gmail.com>
2020-05-08 02:51:51 +0300

    import react libs, render html to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 const App = () => {
-  return <h1>Hello Conrad</h1>
+  return <h1>Hello Conrad.</h1>
 }
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
