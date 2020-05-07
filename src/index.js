1faa2f4870ba14b3fde72e1f7413cd63b1c1289c
conrad96 <conrad.mugisha@gmail.com>
2020-05-08 02:55:35 +0300

    pass className, innerHTML

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <h1>Hello Conrad.</h1>
+  return React.createElement('h1', {className: 'example'}, 'Hello Conrad');
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
