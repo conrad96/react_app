5c20bbd49ec76512a20fd23cc08a767053f3d99d
conrad96 <conrad.mugisha@gmail.com>
2020-05-08 03:12:44 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return React.createElement('h1', {className: 'example'}, 'Hello Conrad');
+  return (
+    <div className="example">
+      <h1>Hello conrad.</h1>
+    </div>
+  )
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
