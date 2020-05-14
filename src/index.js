dd4ea9121747cb237149021e577e8c2affdd3997
conrad96 <conrad.mugisha@gmail.com>
2020-05-14 07:01:09 +0300

    export newslist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import JSON from './db.json';
 
 //Add components here 
 import Header from './components/header'
+import NewsList from './components/news_list';
 
-const App = () => {
+const App = () => {  
+
   return (
     <div className="header">
       <Header />
+      <NewsList />
     </div>
   )
 }
